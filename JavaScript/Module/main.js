80f572c1824c10cccf21780a6e6e69b92709abb3
주상민 <besforyou2@gmail.com>
2023-08-28 17:00:09 +0900

    feat: JavaScript Module
    as 키워드를 통해 import한 식별자명을 변경 가능

---
+++ JavaScript/Module/main.js
@@ -0,0 +1,5 @@
+import {str as xyz, arr, hello as h} from './module.js';
+
+console.log(xyz);
+console.log(arr);
+console.log(h);
---
+++ JavaScript/Module/module.js
@@ -0,0 +1,9 @@
+// 기본 내보내기
+export default 123;
+
+// 이름 내보내기
+export const str = 'ABC';
+export const arr = [1, 2, 3];
+export function hello() {
+  return "Hello world!"
+}
