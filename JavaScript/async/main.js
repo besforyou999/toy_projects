4fbabf6a30f0ddd1883e600e64b05580cb8ff58b
주상민 <besforyou2@gmail.com>
2023-08-28 21:28:40 +0900

    JavaScript - feat: 콜백 지옥 예시

---
+++ JavaScript/async/main.js
@@ -0,0 +1,32 @@
+// 콜백 지옥 예시
+// 1, 2, 3 순서대로 출력하기 위해 콜백을 중첩시키면 가독성이 굉장히 떨어지고 오류 처리도 힘든 코드가 된다.
+const a = (callback) => {
+  setTimeout(() => {
+    console.log(1);
+    callback()
+  }, 1000)
+}
+
+const b = (callback) => {
+  setTimeout(() => {
+    console.log(2);
+    callback();
+  }, 1000)
+}
+
+const c = (callback) => {
+  setTimeout(() => {
+    console.log(3)
+    callback();
+  }, 1000)
+}
+
+const d = () => console.log(4);
+
+a(() => {
+  b(() => {
+    c(() => {
+      d()
+    })
+  })
+})
