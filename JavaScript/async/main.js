0cdd6fc875f8cfe6426879bf7dcc8319ac8152e8
주상민 <besforyou2@gmail.com>
2023-08-28 22:14:10 +0900

    JavaScript - async, await
    Promise의 단점인 좋지 않은 가독성을 async, await을 통해 개선할 수 있다.

--- JavaScript/async/main.js
+++ JavaScript/async/main.js
@@ -11,15 +11,26 @@ const getMovies = movieName => {
   })
 }
 
-getMovies('frozen')
-  .then(() => {
-    console.log('겨울왕국!')
-    return getMovies('avengers')
-  })
-  .then(() => {
-    console.log('어벤져스!')
-    return getMovies('avatar')
-  })
-  .then(() => {
-    console.log('아바타!');
-  })
+// getMovies('frozen')
+//   .then(() => {
+//     console.log('겨울왕국!')
+//     return getMovies('avengers')
+//   })
+//   .then(() => {
+//     console.log('어벤져스!')
+//     return getMovies('avatar')
+//   })
+//   .then(() => {
+//     console.log('아바타!');
+//   })
+
+const wrap = async() => {
+  await getMovies('frozen')
+  console.log('겨울왕국')
+  await getMovies('avengers')
+  console.log('어벤져스!')
+  await getMovies('avatar')
+  console.log('아바타!')
+}
+
+wrap();
