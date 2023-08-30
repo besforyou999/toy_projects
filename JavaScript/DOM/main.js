c63616be54486c78731966ff0153eb48889f38fd
주상민 <besforyou2@gmail.com>
2023-08-30 16:55:58 +0900

    JavaScript - feat: dispatchEvent

--- JavaScript/DOM/main.js
+++ JavaScript/DOM/main.js
@@ -1,27 +1,18 @@
-const formEl = document.querySelector('form');
-const inputEls = document.querySelectorAll('input');
+const child1 = document.querySelector('.child:nth-child(1)')
+const child2 = document.querySelector('.child:nth-child(2)')
 
-inputEls.forEach(el => {
-  el.addEventListener('focus', () => {  // input을 클릭하면 focus가 되어 active 클래스가 붙음
-    formEl.classList.add('active')
-  })
-  el.addEventListener('blur', () => {   // input focus가 사라지는 걸 blur라고 표현
-    formEl.classList.remove('active')
-  })
-  el.addEventListener('input', () => {
-    console.log(event.target.value)
-  })
+child1.addEventListener('click', event => {
+  // 강제로 이벤트 발생
+  child2.dispatchEvent(new Event('click'))
+  child2.dispatchEvent(new Event('wheel'))
+  child2.dispatchEvent(new Event('keydown'))
 })
-
-formEl.addEventListener('submit', event => {
-  event.preventDefault(); // form 요소에서 submit 이벤트가 발생하면 디폴트로 페이지가 새로고침됨. 이를 명시적으로 막을 수 있다.
-  const data = {
-    id: event.target[0].value,
-    pw: event.target[1].value
-  }
-  console.log('제출!', data);
+child2.addEventListener('click', event => {
+  console.log('Child2 Click!')
 })
-
-formEl.addEventListener('reset', event => {
-  console.log('리셋!')
+child2.addEventListener('wheel', event => {
+  console.log('Child2 Wheel!')
+})
+child2.addEventListener('keydown', event => {
+  console.log('Child2 keydown!')
 })
