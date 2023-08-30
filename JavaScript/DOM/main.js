df4d68ca98af783705d5299a7a75567b43f69afd
주상민 <besforyou2@gmail.com>
2023-08-30 14:38:28 +0900

    JavaScript - feat: 이벤트 위임

--- JavaScript/DOM/main.js
+++ JavaScript/DOM/main.js
@@ -1,21 +1,24 @@
-// 핸들러 한 번만 실행
+// 이벤트 위임 (Deligation)
+
+// 단일 조상 요소에서 제어하는 이벤트 위임 패턴
+// 이 방식을 통해 다수의 요소에 이벤트 핸들러를 등록할 필요없이
+// 하나의 조상 요소에만 이벤트 핸들러를 등록하여 사용 가능
+// -> 이벤트 핸들러가 조상 요소 하나에만 등록되어 관리가 편해짐
 
 const parentEl = document.querySelector('.parent')
+const childEls = document.querySelectorAll('.child')
 
-parentEl.addEventListener('click', event => {
-  console.log('Parent!')
-}, {
-  once: true
+// 모든 대상 요소에 이벤트 등록하는 방식
+childEls.forEach(el => {
+  el.addEventListener('click', event => {
+    console.log(event.target.textContent);
+  })
 })
 
-// 기본 동작과 핸들러 실행 분리
-
-// 핸들러의 실행이 많은 PC 자원을 소모해 기본 동작이 버벅일 수 있음
-// passive: true 옵션을 통해 기본 동작과 핸들러 실행을 분리해 위 문제를 해결 가능
-parentEl.addEventListener('wheel', event => {
-  for (let i = 0 ; i < 10000; i += 1) {
-    console.log(i)
+// 조상 요소에 이벤트 위임
+parentEl.addEventListener('click', event => {
+  const childEl = event.target.closest('.child')
+  if (childEl) {
+    console.log(childEl.textContent);
   }
-}, {
-  passive: true
 })
