3c35548b930e73ae70d2fd9c3de46008b4e9b7aa
주상민 <besforyou2@gmail.com>
2023-08-30 18:31:50 +0900

    JavaScript - feat: Storage

--- JavaScript/DOM/main.js
+++ JavaScript/DOM/main.js
@@ -1,16 +1,24 @@
-// Cookie (쿠키)
+// Storage (스토리지)
 
 // 도메인 단위로 저장
-// 표준안 기준, 사이트당 최대 20개 및 4KB 제한
-// 영구 저장 불가능
+// 5MB 제한
+// 세션 혹은 영구 저장 가능
 
-// domain : 유효 도메인 설정
-// path: 유효 경로 설정
-// expires: 만료 날짜 (UTC Date) 설정
-// max-age: 만료 타이머(s) 설정
+// sessionStorage: 브라우저 세션이 유지되는 동안에만 데이터 저장. 브라우저 닫으면 삭제됨
+// localStorage: 따로 제거하지 않으면 영구적으로 데이터 저장
 
-document.cookie = `a=1; max-age=${60 * 60 * 24 * 3}`
-document.cookie = `b=2; expires=${new Date(2022, 11, 16).toUTCString()}`
+// .getItem(): 데이터 조회
+// .setItem(): 데이터 추가
+// .removeItem() : 데이터 제거
+// .clear(): 스토리지 초기화
 
-console.log(document.cookie); 
+localStorage.setItem('a', 'Hello world!')
+localStorage.setItem('b', JSON.stringify({x: 1, y: 2}))
+localStorage.setItem('c', 123)
 
+console.log(localStorage.getItem('a'))
+console.log(JSON.parse(localStorage.getItem('b')))
+console.log(localStorage.getItem('c'))
+
+localStorage.removeItem('a')  // a 제거
+localStorage.clear()          // 전부 제거
