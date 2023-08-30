0fc8a2282f90bb181c394a2c3cca8f4b4e9d1c7c
주상민 <besforyou2@gmail.com>
2023-08-30 16:47:10 +0900

    JavaScript - feat: form 요소 & focus event

--- JavaScript/DOM/main.js
+++ JavaScript/DOM/main.js
@@ -1,18 +1,27 @@
-// Keyboard Events
+const formEl = document.querySelector('form');
+const inputEls = document.querySelectorAll('input');
 
-const inputEl = document.querySelector('input');
-
-inputEl.addEventListener('keydown', event => {
-  if (event.key == 'Enter') {
-    console.log(event.target.value);
-  }
+inputEls.forEach(el => {
+  el.addEventListener('focus', () => {  // input을 클릭하면 focus가 되어 active 클래스가 붙음
+    formEl.classList.add('active')
+  })
+  el.addEventListener('blur', () => {   // input focus가 사라지는 걸 blur라고 표현
+    formEl.classList.remove('active')
+  })
+  el.addEventListener('input', () => {
+    console.log(event.target.value)
+  })
 })
 
-// 한글을 입력할 경우 브라우저 처리 과정이 필요하여 콘솔에 입력한 한글이 두 번 출력된다.
-// 이를 방지하기 위해서 isComposing 속성 사용 가능
-
-inputEl.addEventListener('keydown', event => {
-  if (event.key == 'Enter' && !event.isComposing) {
-    console.log(event.target.value);
+formEl.addEventListener('submit', event => {
+  event.preventDefault(); // form 요소에서 submit 이벤트가 발생하면 디폴트로 페이지가 새로고침됨. 이를 명시적으로 막을 수 있다.
+  const data = {
+    id: event.target[0].value,
+    pw: event.target[1].value
   }
+  console.log('제출!', data);
+})
+
+formEl.addEventListener('reset', event => {
+  console.log('리셋!')
 })
