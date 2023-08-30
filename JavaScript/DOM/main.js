9d8532b5aced0208d400737c00b65b071026f0b8
주상민 <besforyou2@gmail.com>
2023-08-30 14:25:29 +0900

    JavaScript - feat: 이벤트 옵션

--- JavaScript/DOM/main.js
+++ JavaScript/DOM/main.js
@@ -1,26 +1,21 @@
-// 이벤트 캡쳐링
+// 핸들러 한 번만 실행
 
-const parentEl = document.querySelector('.parent');
-const childEl = document.querySelector('.child');
-const anchorEl = document.querySelector('a');
+const parentEl = document.querySelector('.parent')
 
-window.addEventListener('click', event => {
-  console.log('Window!');
-})
-document.addEventListener('click', event => {
-  console.log('Body!');
-}, { capture: true })
 parentEl.addEventListener('click', event => {
-  console.log('Parent!');
-}, { capture: true })
-childEl.addEventListener('click', event => {
-  console.log('Child!');
-})
-anchorEl.addEventListener('click', event => {
-  console.log('Anchor!');
+  console.log('Parent!')
+}, {
+  once: true
 })
 
-// 링크 클릭해보면 Body! > Parent! > Child! > Window! 순으로 출력된다.
-// 이벤트가 루트에서 a 태그까지 내려가면서 document, parent 요소 순으로 캡쳐링되고
-// 이벤트가 a 태그에 다다르면 다시 루트 요소 방향으로 전파되면서 이벤트 핸들러가 실행되게 되는데
-// document, parent 요소에서는 이미 이벤트 캡쳐링 되었으므로 핸들러 실행없이 이벤트만 위로 전파됨.
+// 기본 동작과 핸들러 실행 분리
+
+// 핸들러의 실행이 많은 PC 자원을 소모해 기본 동작이 버벅일 수 있음
+// passive: true 옵션을 통해 기본 동작과 핸들러 실행을 분리해 위 문제를 해결 가능
+parentEl.addEventListener('wheel', event => {
+  for (let i = 0 ; i < 10000; i += 1) {
+    console.log(i)
+  }
+}, {
+  passive: true
+})
