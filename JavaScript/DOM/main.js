d241761a1c9254a939742d316c0d9c17a40aa1ce
주상민 <besforyou2@gmail.com>
2023-08-30 11:48:08 +0900

    JavaScript - feat: addEventListener()

--- JavaScript/DOM/main.js
+++ JavaScript/DOM/main.js
@@ -1,18 +1,18 @@
-// document.getElementById()
-
-// HTML id 속성 값으로 검색한 요소 반환
-// 여러 요소가 검색되면, 가장 먼저 찾은 요소만 반환
-// 검색 결과가 없으면, 'null'을 반환
-const el = document.getElementById('child123');
-console.log(el);
+// addEventListener()
 
 
+const parentEl = document.querySelector('.parent');
+const childEl = document.querySelector('.child');
 
-// document.querySelectorAll()
+parentEl.addEventListener('click', () => {
+  console.log('Parent!');
+})
 
-// 'CSS 선택자'로 검색한 모든 요소를 `NodeList`로 반환
-// `NodeList` 객체는 `.forEach()`를 사용할 수 있습니다.
+childEl.addEventListener('click', () => {
+  console.log('Child!');
+})
 
-const nodeList = document.querySelectorAll('.child');
-console.log(nodeList);
-nodeList.forEach(el => console.log(el.textContent));
+// childEl를 클릭하면 'Child!', 'Parent!' 둘 다 출력된다.
+// parentEl를 클릭하면 'Parent!'만 출력된다.
+
+
