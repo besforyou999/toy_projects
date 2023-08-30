ec4dd8093b9bd7cc95b2724481015a595de71acb
주상민 <besforyou2@gmail.com>
2023-08-30 12:06:35 +0900

    JavaScript - feat: removeEventListener()

--- JavaScript/DOM/main.js
+++ JavaScript/DOM/main.js
@@ -1,18 +1,15 @@
-// addEventListener()
+// .removeEventListener()
 
+// 등록했던 이벤트 핸들러 제거
 
 const parentEl = document.querySelector('.parent');
 const childEl = document.querySelector('.child');
 
-parentEl.addEventListener('click', () => {
+const handler = () => {
   console.log('Parent!');
-})
+}
 
+parentEl.addEventListener('click', handler);
 childEl.addEventListener('click', () => {
-  console.log('Child!');
+  parentEl.removeEventListener('click', handler);
 })
-
-// childEl를 클릭하면 'Child!', 'Parent!' 둘 다 출력된다.
-// parentEl를 클릭하면 'Parent!'만 출력된다.
-
-
