04695e12d4e66a17f39e01706526e62b5340d21c
주상민 <besforyou2@gmail.com>
2023-08-31 16:24:55 +0900

    JavaScript -feat: BigInt

--- JavaScript/Symbol_BigInt/main.js
+++ JavaScript/Symbol_BigInt/main.js
@@ -1,18 +1,17 @@
-// 심볼 (Symbol)
+// BigInt
 
-// 변경이 불가한 데이터로, 유일한 식별자를 만들어 데이터를 보호하는 용도로 사용할 수 있습니다.
+// BigInt는 길이 제한이 없는 정수(integer)입니다.
+// 숫자(number) 데이터가 안정적으로 표시할 수 있는,
+// 최대치(2^53 - 1)보다 큰 정수를 표현할 수 있습니다.
+// 정수 뒤에 `n`을 붙이거나 `BigInt()`를 호출해 생성합니다.
 
-// Symbol ('설명')
-// '설명'은 단순 디버깅을 위한 용도일 뿐, 심볼 값과는 관계가 없습니다. 
+const a = 11n
+const b = 22
 
-import heropy from './heropy.js'
-import { birthKey, emailKey } from './keys.js';
+// 숫자 => BigInt
+console.log(a + BigInt(b))
+console.log(typeof (a + BigInt(b)))
 
-console.log(heropy);
-console.log(Object.keys(heropy));
-for (const key in heropy) {
-  console.log(heropy[key]);
-}
-
-console.log(heropy[birthKey]);
-console.log(heropy[emailKey]);
+// BigInt => 숫자
+console.log(Number(a) + b)
+console.log(typeof (Number(a) + b))
