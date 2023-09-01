fee3e3cf23b564dbc01a33dec746dfbc263ab435
주상민 <besforyou2@gmail.com>
2023-09-01 20:56:55 +0900

    JavaScript - feat: 정규표현식 플래그

--- JavaScript/Regular_Expression/main.js
+++ JavaScript/Regular_Expression/main.js
@@ -1,8 +1,8 @@
 // 정규표현식 (RegExp, Regular Expression)
 
-// 정규식.test(문자열) - 일치 여부 반환
-// 문자열.match(정규식) - 일치하는 문자의 배열 반환
-// 문자열.replace(정규식, 대체문자) - 일치하는 문자를 대체
+// g - 모든 문자 일치 (Global)
+// i - 영어 대소문자를 구분 않고 일치 (Ignore case)
+// m - 여러 줄 일치 (Multi line), 각각의 줄을 시작과 끝으로 인식!
 
 const str = `
 010-3127-7629
@@ -16,8 +16,8 @@ http://localhost:1234
 abbcccddddeeeee
 `
 
-const regexp = /fox/gi
-
-console.log(regexp.test(str));    // 문자열에 'fox'가 있는지 탐색한다.
-console.log(str.match(regexp));   // ['fox']
-console.log(str.replace(regexp, 'cat'));  // str에서 'fox'가 'cat'으로 변경됨
+console.log(str.match(/the/))
+console.log(str.match(/the/g))
+console.log(str.match(/the/gi))
+console.log(str.match(/\.$/gi))
+console.log(str.match(/\.$/gim))
